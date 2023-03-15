1b38ca9e09fa478b1f05c85adab8461b457cfe90
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2023-03-14 21:02:45 -0300

    NEW criacao de api com endereco de requisicao

---
+++ src/utils/api.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from "axios";
+
+export function getApi(ctx?: any): AxiosInstance {
+    const api: AxiosInstance = axios.create({
+        baseURL: 'http://localhost:4500'
+    })
+
+    return api;
+};
