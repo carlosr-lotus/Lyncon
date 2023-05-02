8fc26c9633baa668f048b8c2aec06edf7c36f1ea
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2023-05-02 20:33:05 -0300

    ADD - Type for new params

--- src/pages/carrinho/index.tsx
+++ src/pages/carrinho/index.tsx
@@ -17,6 +17,9 @@ interface ProductCart {
     id: number,
     nameProduct: string,
     priceProduct: number,
+    colorName: string,
+    sizeProduct: string,
+    shippingRate: number,
     imageProduct: string
 }
 
