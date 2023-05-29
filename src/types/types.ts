db20d5c0526b62cf8f2c150f6b47fd9edf1d3351
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2023-05-29 10:51:14 -0300

    UPD - Types for produto page

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,27 @@
+// produto - index.tsx //
+export interface ProductProps {
+    id: number,
+    name: string,
+    pricing: number,
+    image: string,
+    colors: ColorType[],
+    sizes: SizeType[],
+    desc?: string
+}
+
+export interface ColorType {
+    id: number,
+    colorHex: string,
+    colorName: string
+}
+
+export interface SizeType {
+    id: number,
+    size: string,
+    isAvailable: boolean
+}
+
+// carrinho - index.tsx //
 export interface ProductCart {
     id: number,
     nameProduct: string,
