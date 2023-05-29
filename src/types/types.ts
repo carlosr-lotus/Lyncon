8707d522e383dc9bbc3ca68fdb2a227ed775d925
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2023-05-29 10:42:54 -0300

    ADD - Testing separated file for interfaces

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export interface ProductCart {
+    id: number,
+    nameProduct: string,
+    priceProduct: number,
+    colorName: string,
+    sizeProduct: string,
+    shippingRate: number,
+    imageProduct: string
+}
