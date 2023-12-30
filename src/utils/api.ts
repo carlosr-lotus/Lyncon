22baffa106796383b4c9dc742a3911ebebcd4d93
Carlos Lotus <71396696+carlosr-lotus@users.noreply.github.com>
2023-12-30 03:12:42 -0300

    ADD - semicolon

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from "axios";
 export function getApi(ctx?: any): AxiosInstance {
     const api: AxiosInstance = axios.create({
         baseURL: 'http://localhost:4500'
-    })
+    });
 
     return api;
 };
