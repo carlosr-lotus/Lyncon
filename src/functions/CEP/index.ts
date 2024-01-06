e9647fde6580481370b2c393cea399b381af8721
Carlos Lotus <71396696+carlosr-lotus@users.noreply.github.com>
2024-01-06 03:20:38 -0300

    ADD - cep function
    Stored functions related to CEP (standard brazilian zip code).

---
+++ src/functions/CEP/index.ts
@@ -0,0 +1,18 @@
+// This file is used to valid CEP format code which is a brazilian zip code
+// Example format: 12345-123
+
+type CepFunctions = {
+    isCepValid: (cep: string) => boolean // Check if cep string is valid
+}
+
+function isCepValid(cep: string): boolean {
+    const pattern: RegExp = /^[0-9]{5}-[0-9]{3}$/;
+
+    if (pattern.test(cep)) 
+        return true 
+    else return false
+};
+
+export const CEP: CepFunctions = {
+    isCepValid: isCepValid
+}
