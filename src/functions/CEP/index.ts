07756bff3c42c00ee02aaa585555fdf31e073d47
carlosr-lotus <carlosr_juniorpro@protonmail.com>
2024-02-09 19:40:30 -0300

    Teste commit cmd

--- src/functions/CEP/index.ts
+++ src/functions/CEP/index.ts
@@ -1,18 +1,19 @@
 // This file is used to valid CEP format code which is a brazilian zip code
 // Example format: 12345-123
-
 type CepFunctions = {
-    isCepValid: (cep: string) => boolean // Check if cep string is valid
+    isValid: (cep: string) => boolean // Check if cep string is valid
 }
 
-function isCepValid(cep: string): boolean {
+function isValid(cep: string): boolean {
     const pattern: RegExp = /^[0-9]{5}-[0-9]{3}$/;
 
     if (pattern.test(cep)) 
-        return true 
+        return true; 
     else return false;
 };
 
-export const CEP: CepFunctions = {
-    isCepValid: isCepValid
+const CEP: CepFunctions = {
+    isValid: isValid
 }
+
+export default CEP;
