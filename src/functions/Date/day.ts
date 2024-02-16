d2ca36946eb9faac9e9fc300ecbec52d9b72f46c
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-02-16 11:10:02 -0300

    import types

--- src/functions/Date/day.ts
+++ src/functions/Date/day.ts
@@ -1,51 +1,49 @@
+import { DaysWeekPtBR } from "./Day/types";
+
 type DayFunctions = {
-    getDayOfWeek: (date: Date) => string,
-    today: () => Date,
-    tomorrow: (date?: Date) => Date,
-    yesterday: (date?: Date) => Date,
-}
+  getDayOfWeek: (date: Date) => string;
+  today: () => Date;
+  tomorrow: (date?: Date) => Date;
+  yesterday: (date?: Date) => Date;
+};
 
 function getDayOfWeek(date: Date): string {
-    const days: string[] = [
-        'Domingo',
-        'Segunda-feira',
-        'Terça-feira',
-        'Quarta-feira',
-        'Quinta-feira',
-        'Sexta-feira',
-        'Sábado'
-    ];
+  const days: DaysWeekPtBR[] = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+  ];
 
-    return days[date.getDay()];
+  return days[date.getDay()];
 }
 
 function yesterday(date?: Date): Date {
-    const yesterday = new Date(date?.getTime() ?? new Date().getTime());
-    yesterday.setDate(
-        (date?.getDate() ?? new Date().getDate()) - 1
-    );
+  const yesterday = new Date(date?.getTime() ?? new Date().getTime());
+  yesterday.setDate((date?.getDate() ?? new Date().getDate()) - 1);
 
-    return yesterday;
+  return yesterday;
 }
 
 function today(): Date {
-    return new Date();
+  return new Date();
 }
 
 function tomorrow(date?: Date): Date {
-    const tomorrow = new Date(date?.getTime() ?? new Date().getTime());
-    tomorrow.setDate(
-        (date?.getDate() ?? new Date().getDate()) + 1
-    );
+  const tomorrow = new Date(date?.getTime() ?? new Date().getTime());
+  tomorrow.setDate((date?.getDate() ?? new Date().getDate()) + 1);
 
-    return tomorrow;
+  return tomorrow;
 }
 
 const Day: DayFunctions = {
-    getDayOfWeek: getDayOfWeek,
-    yesterday: yesterday,
-    tomorrow: tomorrow,
-    today: today
-}
+  getDayOfWeek: getDayOfWeek,
+  yesterday: yesterday,
+  tomorrow: tomorrow,
+  today: today,
+};
 
 export default Day;
