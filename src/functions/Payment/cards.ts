fce488701d210413a0ce4cbe98bbdaf39fa75c95
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-02-16 20:06:35 -0300

    ADD - format & valid functions
    Two functions to now format card number and check if it's valid.

--- src/functions/Payment/cards.ts
+++ src/functions/Payment/cards.ts
@@ -1,5 +1,8 @@
+// Functions related to credit/debit cards.
 type CardsFunctions = {
-    getIdentifier: (cardNumber: number) => string
+    getIdentifier:      (cardNumber: number) => string,
+    formatCardNumber:   (cardNumber: number) => string,
+    isValid:            (cardNumber: number) => boolean
 }
 
 function getIdentifier(cardNumber: number): 
@@ -8,7 +11,7 @@ function getIdentifier(cardNumber: number):
     'visa'             |
     'none'
 {
-    const cardFirstNumber: number = Number(cardNumber.toString().charAt(0));
+    const cardFirstNumber = Number(cardNumber.toString().charAt(0));
 
     if (cardFirstNumber === 3)
         return 'american express';
@@ -22,8 +25,31 @@ function getIdentifier(cardNumber: number):
     return 'none';
 }
 
+function isValid(cardNumber: number): boolean {
+    const pattern: RegExp = /^[0-9]{4}[0-9]{4}[0-9]{4}[0-9]{4}$/;
+
+    console.log(cardNumber.toString());
+
+    if (pattern.test(cardNumber.toString()) && getIdentifier(cardNumber) !== 'none')
+        return true;
+    else return false;
+}
+
+function formatCardNumber(cardNumber: number): string {
+    const pattern: RegExp = /(\d{4})(?=\d)/g;
+
+    if (isValid(cardNumber)) {
+        console.log(cardNumber.toString().replace(pattern, '$1.'));
+        return cardNumber.toString().replace(pattern, '$1.');
+    } else {
+        return 'invalid';
+    }
+}
+
 const Card: CardsFunctions = {
-    getIdentifier: getIdentifier
+    getIdentifier: getIdentifier,
+    formatCardNumber: formatCardNumber,
+    isValid: isValid
 }
 
 export default Card;
