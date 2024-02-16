b2d1f85e82f677ca9bba1c8e24199b2bda7c01ea
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-02-16 11:10:48 -0300

    types for Days related functions

---
+++ src/functions/Date/Day/types.ts
@@ -0,0 +1,17 @@
+export type DaysWeekPtBR =
+  | "Domingo"
+  | "Segunda-feira"
+  | "Terça-feira"
+  | "Quarta-feira"
+  | "Quinta-feira"
+  | "Sexta-feira"
+  | "Sábado";
+
+export type DaysWeekUsEN =
+  | "Sunday"
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday";
