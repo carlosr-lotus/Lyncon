15e741f076a042f3ff56a0b3c0e3843a02a02aac
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-02-16 20:05:29 -0300

    UPD - New props

--- src/components/material/InputField.tsx
+++ src/components/material/InputField.tsx
@@ -1,16 +1,19 @@
-import { CSSProperties, ChangeEvent, LegacyRef, FormEvent } from "react";
+import { CSSProperties, ChangeEvent, LegacyRef, FormEvent, KeyboardEvent } from "react";
 import styles from '../../styles/components/materialStyle/InputField.module.css';
 
 type InputFieldProps = {
     name: string,
     type: 'text' | 'password' | 'number',
     required?: boolean,
-    placeholder: string,
+    placeholder?: string,
+    pattern?: string,
     className?: string,
+    defaultValue?: string,
     ref?: LegacyRef<HTMLInputElement> | undefined,
     register: any,
     onChange?: (e: ChangeEvent<HTMLInputElement>) => void,
     onInput?: (e: FormEvent<HTMLInputElement>) => void,
+    onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void,
     style?: CSSProperties
 }
 
@@ -19,12 +22,15 @@ export default function InputField(
         name,
         type,
         placeholder,
+        pattern,
         required,
         className,
+        defaultValue,
         ref,
         register,
         onChange,
         onInput,
+        onKeyDown,
         style
     }: InputFieldProps): JSX.Element {
 
@@ -33,8 +39,11 @@ export default function InputField(
             name={name}
             type={type}
             placeholder={placeholder}
+            pattern={pattern}
             className={className ? className : styles.inputFieldBorderAround}
             onChange={onChange}
+            onKeyDown={onKeyDown}
+            defaultValue={defaultValue}
             style={style}
             ref={ref}
             onInput={(e) => onInput ? onInput(e) : null}
